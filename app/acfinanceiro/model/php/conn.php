3e700e6cb722837f867bd130828aa396fb6c858d
Andre Assunção <104846613+AndreAssun@users.noreply.github.com>
2023-08-14 21:39:27 -0300

    att

---
+++ app/acfinanceiro/model/php/conn.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "mysql.professorcelso.com.br";
+$username = "professorcelso";
+$password = "Senac2023";
+$dbname = "professorcelso";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
